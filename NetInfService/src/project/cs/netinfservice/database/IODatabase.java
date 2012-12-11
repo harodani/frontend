b86eefd5c3c8c1ba52f6b8a11b6053c7c1c30784
Thiago Costa Porto <thiagocostaporto@gmail.com>
2012-12-11 10:07:02 +0100

    IODatabase: 	-- Javadoc 	-- Added some TODO's to be checked later.
    Signed-off-by: Thiago Costa Porto <thiagocostaporto@gmail.com>

--- NetInfService/src/project/cs/netinfservice/database/IODatabase.java
+++ NetInfService/src/project/cs/netinfservice/database/IODatabase.java
@@ -121,9 +121,10 @@ public class IODatabase
 	/**
 	 * Creates a new Database for storing IO information.
 	 * 
-	 * @param context			The application context
-	 * @param datamodelFactory	The factory that is used in order to 
-	 * 							create information objects.
+	 * @param context
+	 * 		The application context
+	 * @param datamodelFactory
+	 *     	The factory that is used in order to create information objects.
 	 */
 	@Inject
 	public IODatabase(DatamodelFactory datamodelFactory, @Assisted Context context) {
@@ -304,14 +305,16 @@ public class IODatabase
 		}
 	}
 	
-	// TODO: Maybe this should raise an exception? Writing might not be available.
 	/**
 	 * Inserts a value in the database.
 	 * 
-	 * @param table		the table where the values will be inserted
-	 * @param values	the values that will be inserted
+	 * @param table
+     *      The table where the values will be inserted
+	 * @param values
+	 *      The values that will be inserted
 	 */
 	private void insert(String table, ContentValues values) {
+	    // TODO: Maybe this should raise an exception? Writing might not be available.
 		// Get database
 	    SQLiteDatabase db = this.getWritableDatabase();
 		
@@ -325,19 +328,24 @@ public class IODatabase
 	/**
 	 * Returns the list of URL associated with a hash.
 	 * 
-	 * @param hash	a hash from an information object
-	 * @return		the list of URL in the IO_url table 
-	 * 				corresponding to the given hash
+	 * @param hash
+	 *     A hash from an information object
+	 * @return
+     *     The list of URL in the IO_url table corresponding to the given hash
 	 */
 	public List<String> getURLs(String hash) {
 		List<String> urlList = new ArrayList<String>();
 
 		Cursor cursor = null;
+		
+		// Make database query
 		try {
 			cursor = query(TABLE_URL, KEY_HASH, hash);
 		} catch (DatabaseException e) {
 			return urlList;
 		}
+		
+		// add all results to list
 		do {
 			urlList.add(cursor.getString(1));
 		} while (cursor.moveToNext());
@@ -348,35 +356,47 @@ public class IODatabase
 	/**
 	 * Returns the information object specified by the hash value, if existent.
 	 * 
-	 * @param hash					The hash value identifying the information object
-	 * @return						The information object
-	 * @throws DatabaseException 	Thrown when the query does not return any value
+	 * @param hash
+	 *     The hash value identifying the information object
+	 * @return
+	 *     The information object
+	 * @throws DatabaseException
+	 *     Thrown when the query does not return any value
 	 */
 	public InformationObject getIO(String hash) throws DatabaseException {
 		Log.d(TAG, "Searching for information object.");
+		
+		// Query. If it fails, it will throw a DatabaseException
 		Cursor cursor = query(TABLE_IO, KEY_HASH, hash);
 		
 		Log.d(TAG, "Found information object.");
 		
         Log.d(TAG, "Sending Intent " + LOCAL_TRANSMISSION);
-        Intent intent = new Intent(LOCAL_TRANSMISSION);
+        
+        // Create and send an intent for local transmission
+        Intent intent = new Intent(LOCAL_TRANSMISSION);        
         MainNetInfActivity.getActivity().sendBroadcast(intent);
 
-		
+        // Build a new IO to host the result
 		IOBuilder builder = new IOBuilder(mDatamodelFactory);
+		
+		// Populate the fields of the IO
 		builder.setHash(cursor.getString(0))
 			.setHashAlgorithm(cursor.getString(1))
 			.setContentType(cursor.getString(2))
 			.addFilePathLocator(cursor.getString(3))
 			.addMetaData(mFilepathLabel, cursor.getString(3))
 			.addMetaData(mFilesizeLabel, cursor.getString(4));
-		
+
+		// Get URLs related to the hash
 		cursor = query(TABLE_URL, KEY_HASH, hash);
 		
+		// Add URLs to metadata
 		do {
 			builder.addMetaData(mUrlLabel, cursor.getString(1));
 		} while (cursor.moveToNext());
 		
+		// Return the Information Object built
 		return builder.build();
 	}
 	
@@ -384,12 +404,16 @@ public class IODatabase
 	 * Returns the information object corresponding to the url,
 	 * if existent.
 	 * 
-	 * @param url					The url that identifies the information object
-	 * @return						The information object
-	 * @throws DatabaseException	Is thrown if the url doesn't belong 
-	 * 								to any stored information object
+	 * @param url
+     *      The url that identifies the information object
+	 * @return
+	 * 		The information object
+	 * @throws DatabaseException
+	 *     	Is thrown if the url doesn't belong to any stored information object 
 	 */
-	public SearchResult searchIO(String url) throws DatabaseException {		
+	@SuppressWarnings("unchecked") // Because of urlArray.add(cursor..)
+    public SearchResult searchIO(String url) throws DatabaseException {
+	    // Metadata holder. Uses our Metadata class
 		Metadata metadata = new Metadata();
 		
 		// Find the hash identification of the corresponding object
@@ -410,153 +434,202 @@ public class IODatabase
 		metadata.insert(mFilepathLabel, cursor.getString(3));
 		metadata.insert(mFilesizeLabel, cursor.getString(4));
 		
-		// Get the algorithm
+		// Get the hash algorithm used to create the object's hash
 		String hashAlg = cursor.getString(1);
 
+		// Return a SearchResult object, accessible through the SearchResult interface
 		return new SearchResultImpl(hash, hashAlg, metadata);
 	}
 
 	/**
-	 * Deletes the information object corresponding to 
-	 * the specified hash value from the database.
+	 * Deletes the information object corresponding to the specified hash value from the database. 
 	 * 
-	 * @param hash	The hash value identifying the information object.
+	 * @param hash
+	 *      The hash value identifying the information object.
 	 */
 	public void deleteIO(String hash) {
+	    // TODO: Check what to do when it fails, as getWritableDatabase throws SQLiteException. 
 		Log.d(TAG, "Deleting io corresponding to the following hash: " + hash);
 		SQLiteDatabase db = getWritableDatabase();
 		
+		// Finds and deletes object
 		db.delete(TABLE_IO, KEY_HASH + " = ?", new String[] {hash});
+		
 		db.close();
 	}
 	
 	/**
-	 * Deletes the information object that is specified
-	 * from the database.
+	 * Deletes the information object that is specified from the database.
 	 * 
-	 * @param io	The information object to delete.
+	 * @param io
+	 *      The information object to delete.
 	 */
 	public void deleteIO(InformationObject io) {
 		Log.d(TAG, "Deleting an information object from the database.");
 		
+		// Get the Identifier from the InformationObject
 		Identifier identifier = io.getIdentifier();
+		
+		// Extract the hash
 		String hash = identifier.getIdentifierLabel(
 				SailDefinedLabelName.HASH_CONTENT.getLabelName()).getLabelValue();
+		
+		// Delete the object by calling deleteIO(hash)
 		deleteIO(hash);
 	}
 	
 	/**
-	 * Returns the map corresponding to the meta data contained in the
-	 * specified metadata String.
+	 * Returns the map corresponding to the meta data contained in the specified Metadata String.
 	 * 
-	 * @param metadata				The complete meta-data String
-	 * @return						A map containing the meta data key value pairs
-	 * @throws DatabaseException	Thrown, if a failure occured during extracting
+	 * @param metadata
+	 *      The complete meta-data String
+	 * @return
+	 * 		A map containing the meta data key value pairs
+	 * @throws DatabaseException
+	 *     	Thrown, if a failure occurred during extracting
 	 */
 	private Map<String, Object> extractMetaData(String metadata) throws DatabaseException {
+	    // Parse metadata to create an JSON Object.
 		Object jsonObject = JSONValue.parse(metadata);
+		
+		// If we could not get a JSONObject from the metadata, throw DatabaseException
 		if (!(jsonObject instanceof JSONObject)) {
 			Log.e(TAG, "Invalid metadata.");
+			
+			// Throw exception
 			throw new DatabaseException(
 					"Metadata in the information object was an invalid JSONObject.");
 		}
 		
-		Map<String, Object> metadataMap = null;	
+		// Create mapping for metadata. Object may be JSON Object or JSON Array
+		Map<String, Object> metadataMap = null;
+		
+		
+		// Parse metadata again, this time adding objects to the map to reflect "String" : "Value" 
 		try {
 			metadataMap = MetadataParser.toMap((JSONObject) jsonObject);
 		} catch (JSONException e) {
+		    // TODO: Uses JSON Exception which is deprecated
 			Log.e(TAG, "Error extracting metadata");
 			throw new DatabaseException("The IO cannot be inserted into the database. "
 					+ "Because the meta-data could not be extracted.", e);
-		}	
+		}
+		
+		// Return metadata Map (String, Object)
 		return metadataMap;
 	}
 	
 	/**
 	 * Returns a content value object representing an entry in the IO table.
 	 * 
-	 * @param hash			The hash value of the IO
-	 * @param hashAlgorithm The hash algorithm
-	 * @param contentType	The content type
-	 * @param filePath		The file path
-	 * @param fileSize		The file size
-	 * @return				The corresponding content value
+	 * @param hash
+	 * 		The hash value of the IO
+	 * @param hashAlgorithm
+	 *      The hash algorithm
+	 * @param contentType
+	 *     	The content type
+	 * @param filePath
+	 *     	The file path
+	 * @param fileSize
+	 *     	The file size
+	 * @return
+	 * 		The corresponding content value
 	 */
 	private ContentValues createIOEntry(
 			String hash, String hashAlgorithm, String contentType, 
 			String filePath, String fileSize) {
-		
+		// Create a new ContentValues object, readable by ContentResolver
 		ContentValues ioEntry = new ContentValues();
 		
+		// Add attributes
 		ioEntry.put(KEY_HASH, hash);
 		ioEntry.put(KEY_HASH_ALGORITHM, hashAlgorithm);
 		ioEntry.put(KEY_CONTENT_TYPE, contentType);
-		
 		ioEntry.put(KEY_FILEPATH, filePath);
 		ioEntry.put(KEY_FILE_SIZE, fileSize);
 		
+		// Return the ContentValues with attributes
 		return ioEntry;
 	}
 	
 	/**
 	 * Returns a content value object representing an entry in the IO_url table.
 	 * 
-	 * @param hash	The hash value of the IO
-	 * @param url	The url where it can be found
-	 * @return		The corresponding content value
+	 * @param hash
+	 *     	The hash value of the IO
+	 * @param url
+	 *     	The url where it can be found
+	 * @return
+	 * 		The corresponding content value
 	 */
 	private ContentValues createUrlEntry(String hash, String url) {
+	    // Create new ContentValues object, which is processed by ContentResolver
 		ContentValues urlEntry = new ContentValues();	
+
+		// Add attributes
 		urlEntry.put(KEY_HASH, hash);
 		urlEntry.put(KEY_URL, url);
+		
+		// Return ContentValues with attributes
 		return urlEntry;
 	}
 	
 	/**
-	 * Querys the database and returns a cursor.
+	 * Queries the database and returns a cursor.
 	 * 
-	 * @param table					The table in which we want to query
-	 * @param key					The key
-	 * @param value					The corresponding value
-	 * @return						A cursor pointing to the first row of results
-	 * @throws DatabaseException	Thrown, if no entry was found for the specified key value pair
+	 * @param table
+	 * 		The table in which we want to query
+	 * @param key
+	 * 		The key
+	 * @param value
+	 * 		The corresponding value
+	 * @return
+	 * 		A cursor pointing to the first row of results
+	 * @throws DatabaseException
+	 *     	Thrown, if no entry was found for the specified key value pair
 	 */
 	private Cursor query(String table, String key, String value) throws DatabaseException {
+	    // TODO: This might throw a SQLiteException, which we must handle!
 		SQLiteDatabase db = this.getReadableDatabase();
 
+		// Makes the query for (key,value)
 		Cursor cursor = db.query(table, null, key + "=?", 
 				new String[]{value}, null, null, null);
 		
+		// If there was any result, move the cursor to the first result
 		if (cursor != null && cursor.getCount() != 0) {
 			cursor.moveToFirst();
 		} else {
+		    // Fails if it does not find anything
 		    db.close();
 			throw new DatabaseException("The given key does not correspond to any IO : " + key);
 		}
+		
 		db.close();
 		
+		// Return first object
 		return cursor;
 	}
 	
 	/**
 	 * Query the database to see if a hash already exists.
 	 * 
-	 * @param hash 	The hash of an information object
-	 * @return		false if the hash does not exist
-	 *				true if it does
+	 * @param hash
+	 *     	The hash of an information object
+	 * @return
+	 * 		false if the hash does not exist,
+	 *		true  if it does
 	 */
 	private boolean containsIO(String hash) {
 		try {
-			//Check if the IO we want to insert already exists
+			// Check if the IO we want to insert already exists
 			query(TABLE_IO, KEY_HASH, hash);
 		} catch (DatabaseException e) {
-			/* 
-			 * This exception is thrown if the query is empty, 
-			 * in that case we say that the IO is not stored
-			 */
+			// This exception is thrown if the query is empty, in that case we say that 
+		    // the IO is not stored
 			return false;
 		}
+
 		return true;
 	}
-
 }
